0cecfd4243262dad672bb88e034efff832d318f2
Simplex <kontakt.simplex@pm.me>
2025-01-02 03:48:46 +0100

    fix: 🐛 Fix Antispam

--- util/antispam.py
+++ util/antispam.py
@@ -10,33 +10,36 @@ cache = {}
 class Antispam:
     def __init__(self):
         self.antispam_guilds = None
+        self.BLOCK_COMMANDS = False
         
         if not cache.get("guilds"):
             cache["guilds"] = {}
             
         self.antispam_guilds = cache["guilds"]
     
-    def _check(self, guild_id):
+    def _add_point(self, guild_id, points: int=1):
         if not self.antispam_guilds.get(guild_id):
             self.antispam_guilds[guild_id] = {
                 "last_message": datetime.now(),
-                "count": 0,
+                "messages": 0,
             }
-    
-    def _add_point(self, guild_id, points: int=1):
-        self._check(guild_id)
         
         antispam_guild = self.antispam_guilds[guild_id]
         last_message = antispam_guild["last_message"]
-        
+        messages = antispam_guild["messages"]
+
+        if messages >= MAX_MESSAGES:
+            self.BLOCK_COMMANDS = True
+
         if (datetime.now() - last_message).seconds >= TIME_WINDOW_SECS:
+            print("Lifted")
+            self.BLOCK_COMMANDS = False
             del cache["guilds"][guild_id]
+            return
         
-        antispam_guild["count"] += 1
+        self.antispam_guilds[guild_id]["last_message"] = datetime.now()
+        antispam_guild["messages"] += 1
         
     async def spamming(self, message: discord.Message) -> bool:
-        self._add_point(message.guild.id)                
-        self._check(message.guild.id)
-        if self.antispam_guilds[message.guild.id]["count"] >= MAX_MESSAGES:
-            return True
-        return False
+        self._add_point(message.guild.id)
+        return self.BLOCK_COMMANDS
