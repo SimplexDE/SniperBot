35918edd1b9fc1018caf5d90877c7b470ba96abe
Simplex <kontakt.simplex@pm.me>
2025-04-25 01:21:29 +0200

    fix: 🐛 Errorhandling was sorted in the wrong order

--- util/errorhandling.py
+++ util/errorhandling.py
@@ -3,17 +3,11 @@ from discord import app_commands
 from util.logger import log
 
 async def handle_error(interaction: discord.Interaction, error: Exception):
-        if isinstance(error, app_commands.errors.CheckFailure):
-            await interaction.response.send_message("You are not allowed to use this command.", ephemeral=True)
-            return
-        elif isinstance(error, app_commands.errors.BotMissingPermissions):
+        if isinstance(error, app_commands.errors.BotMissingPermissions):
             await interaction.response.send_message("I am missing permissions to execute this command.", ephemeral=True)
             return
         elif isinstance(error, app_commands.errors.MissingPermissions):
             await interaction.response.send_message("You are missing permissions to execute this command", ephemeral=True)
-            return
-        elif isinstance(error, app_commands.errors.CommandInvokeError):
-            await interaction.response.send_message("Something went wrong, check your arguments.", ephemeral=True)
             return
         elif isinstance(error, app_commands.errors.CommandOnCooldown):
             await interaction.response.send_message("You are currently on cooldown.", ephemeral=True)
@@ -22,8 +16,11 @@ async def handle_error(interaction: discord.Interaction, error: Exception):
             await interaction.response.send_message("Something went wrong, try again.", ephemeral=True)
             log.execption(error)
             return
-        elif isinstance(error, app_commands.errors.BotMissingPermissions):
-            await interaction.response.send_message("I am missing permissions to execute this command.", ephemeral=True)
+        elif isinstance(error, app_commands.errors.CommandInvokeError):
+            await interaction.response.send_message("Something went wrong, check your arguments.", ephemeral=True)
+            return
+        elif isinstance(error, app_commands.errors.CheckFailure):
+            await interaction.response.send_message("You are not allowed to use this command.", ephemeral=True)
             return
         else:
             await interaction.response.send_message("Unknown error, please contact the developer about this.", ephemeral=True)
